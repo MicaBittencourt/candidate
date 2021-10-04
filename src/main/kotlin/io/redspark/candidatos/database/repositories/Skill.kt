5a809f93e1ad9c4a53d5cebbc6b5192d51df81e7
MicaBittencourt <63079757+MicaBittencourt@users.noreply.github.com>
2021-10-04 15:57:33 -0300

    Atualizaqção final

--- src/main/kotlin/io/redspark/candidatos/database/repositories/Skill.kt
+++
@@ -1,6 +0,0 @@
-package io.redspark.candidatos.database.repositories
-
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
-
-interface Skill : JpaRepository<Skill, UUID>
