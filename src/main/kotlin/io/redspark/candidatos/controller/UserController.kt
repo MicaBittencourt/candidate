6b478f35e643aa54de01d4240cfe4d965a42dfee
Paloma Luiz dos Santos Toledo <paah_toledo@hotmail.com>
2021-10-05 13:55:38 -0300

    alteraçoes feitas

--- src/main/kotlin/io/redspark/candidatos/controller/UserController.kt
+++ src/main/kotlin/io/redspark/candidatos/controller/UserController.kt
@@ -3,14 +3,11 @@ package io.redspark.candidatos.controller
 import io.redspark.candidatos.models.dtos.UserListDTO.UserDTO
 import io.redspark.candidatos.service.UserService
 import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.server.ResponseStatusException
 import javax.validation.Valid
 import javax.validation.constraints.Email
 
@@ -25,12 +22,7 @@ class UserController(
         @GetMapping("me")
         fun getUser(@RequestHeader(HttpHeaders.AUTHORIZATION) @Email @Valid email: String): UserDTO?{
 
-
-                val userDTO: UserDTO? = userService.getUser(email)
-                if (userDTO?.email == ""){
-                        throw ResponseStatusException(HttpStatus.NOT_FOUND,"email.not.empty")
-                }
-                return userDTO
+                return userService.getUser(email)
 
         }
 
--- src/main/kotlin/io/redspark/candidatos/service/UserServiceImpl.kt
+++ src/main/kotlin/io/redspark/candidatos/service/UserServiceImpl.kt
@@ -1,9 +1,12 @@
 package io.redspark.candidatos.service
 
 import io.redspark.candidatos.models.dtos.UserListDTO.UserDTO
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.server.ResponseStatusException
 import java.util.*
+import javax.validation.constraints.Email
 
 
 @Service
@@ -12,8 +15,9 @@ class UserServiceImpl(
 ): UserService {
     override fun getUser(email: String): UserDTO {
 
+
         if ( email == ""){
-            return UserDTO(email = "", admin = false)
+            throw ResponseStatusException(HttpStatus.NOT_FOUND,"email.not.empty")
         }
 
         if (email.contains(".admin")) {
