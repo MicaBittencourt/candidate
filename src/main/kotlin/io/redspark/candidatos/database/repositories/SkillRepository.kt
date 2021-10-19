376ee8c48284c52904d94a96287b1cf129947c6e
Paloma Luiz dos Santos Toledo <paah_toledo@hotmail.com>
2021-10-19 12:47:13 -0300

    add endpoint skill

--- src/main/kotlin/io/redspark/candidatos/database/repositories/SkillRepository.kt
+++ src/main/kotlin/io/redspark/candidatos/database/repositories/SkillRepository.kt
@@ -2,10 +2,7 @@ package io.redspark.candidatos.database.repositories
 
 import io.redspark.candidatos.database.entities.Skill
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
 
 interface SkillRepository : JpaRepository<Skill, Long> {
-
-
 
 }
