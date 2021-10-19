d56fb127235d2fc3c5c9127f24d907156266e002
Ligiane Alves Souza Domingos <ligiane.alves.souza@gmail.com>
2021-10-19 08:27:41 -0300

    add endpoint candidates

--- src/main/kotlin/io/redspark/candidatos/modules/candidate/service/CandidateServiceImpl.kt
+++ src/main/kotlin/io/redspark/candidatos/modules/candidate/service/CandidateServiceImpl.kt
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 import java.util.*
 
+// TODO - tratar execeçoes de campos nulos id no update
 //TODO - verificar regras para cadastro de skill caso tenha responsabilidade no cadastro do candidato, com existsById
 //TODO - verificar como será vinculado os dados do candidato nas etapas do processo seletivo
 @Service
@@ -50,9 +51,6 @@ class CandidateServiceImpl(
     }
 
     override fun updateCandidate(id: UUID, updateCandidateDTO: UpdateCandidateDTO) {
-        if (updateCandidateDTO.id == null || candidatesRepository.existsById(updateCandidateDTO.id).not()) {
-            throw ServiceException(ServiceError.CANDIDATE_NOT_FOUND)
-        }
 
         candidatesRepository.findById(id)
             .map { candidate ->
